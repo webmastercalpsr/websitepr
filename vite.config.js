5bdf5c78bbe69b41c80daa66278979386e68149f
ria <ria.gulati@berkeley.edu>
2026-01-26 16:58:24 -0800

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/',                  // custom domain
+  base: '/websitetpr/',                  // custom domain
   build: { outDir: 'docs' },  // <-- important
   plugins: [react()]
 })
