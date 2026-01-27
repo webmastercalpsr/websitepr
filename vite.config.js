70edbd998c0b9f070800d061852b021b3887d3f5
ria <ria.gulati@berkeley.edu>
2026-01-26 17:19:07 -0800

    Fix base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/websitetpr/',                  // custom domain
+  base: '/',                  // custom domain
   build: { outDir: 'docs' },  // <-- important
   plugins: [react()]
 })
